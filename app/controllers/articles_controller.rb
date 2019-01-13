762ade591b5db978003949cc8ac239257db9ed8e
ygtiwari <ygt.blueray@gmail.com>
2019-01-13 20:36:51 +0530

    fixed article show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,6 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
         @comment = Comment.new
-        @comment.article_id = @article.id
     end
 
     def new 
