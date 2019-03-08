392fc8857bc05d65861006c84ad14f5da0521748
yashtiwari <ygt.blueray@gmail.com>
2019-03-08 20:37:25 +0530

    added some commits

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,4 +14,5 @@ class Article < ApplicationRecord
         new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
         self.tags = new_or_found_tags
     end
+    # added some comments
 end
